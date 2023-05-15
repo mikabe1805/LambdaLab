55aa97b8a68dda5cdfd166571002a0b0a4bc0b4f
MIKA BE <mikushbe@gmail.com>
2023-05-14 22:41:20 -0400

    progress on step 5-9

--- Parser.java
+++ Parser.java
@@ -4,20 +4,35 @@ import java.util.ArrayList;
 
 public class Parser {
 	
+	public static class Node{
+		Variable data;
+		Node left;
+		Node right;
+		public Node(Variable data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	public Node root = null;
+	
 	/*
 	 * Turns a set of tokens into an expression.  Comment this back in when you're ready.
 	 */
 	public Expression parse(ArrayList<String> tokens) throws ParseException {
-		Variable var = new Variable(tokens.get(0));
-		
-		// This is nonsense code, just to show you how to thrown an Exception.
-		// To throw it, type "error" at the console.
-		if (var.toString().equals("error")) {
-			throw new ParseException("User typed \"Error\" as the input!", 0);
+		if (tokens.get(0).equals("(")) {
+			int i = 0;
+			ArrayList<String> right = new ArrayList<String>();
+			while (!tokens.get(i).equals(")")) {
+				right.add(tokens.get(i));
+				i++;
+			}
+			
 		}
-		
-		return var;
+		return (Expression) root;
 	}
+	
+	//private Variable order ()
 	
 	public ArrayList<String> preParse(ArrayList<String> tokens) {
 		int n;
